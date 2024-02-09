6a1edfc321648b4ba4d4408edb4a24281e9679d9
Samuel Gesang <sgesang@mailo.com>
2024-02-09 13:37:33 +0100

    Delete Recap/wheater-activities directory

--- Recap/wheater-activities/src/Components/RefreshCountdown/index.js
+++
@@ -1,5 +0,0 @@
-import "./RefreshCountdown.css";
-
-export default function RefreshCountdown({ timeToRefresh }) {
-  return <span className="refresh-countdown">resfreshing in {timeToRefresh} seconds...</span>;
-}
--- Recap/wheater-activities/src/Components/WeatherDisplay/index.js
+++
@@ -1,5 +0,0 @@
-import "./WeatherDisplay.css";
-
-export default function WeatherDisplay({ isGoodWeather }) {
-  return <h2>{isGoodWeather ? "Good Weather 🌤️" : "Bad Weather 🌧️"}</h2>;
-}
