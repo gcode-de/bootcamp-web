aa9b1d7d03da618ad28be0b10fa9e14cec77ee21
Samuel Gesang <sgesang@mailo.com>
2024-02-16 11:22:13 +0100

    add SWR ex1

---
+++ NextJS/react-data-fetching_iss-tracker/components/ISSTracker/index.js
@@ -0,0 +1,21 @@
+import Controls from "../Controls/index";
+import Map from "../Map/index";
+import useSWR from "swr";
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const URL = "https://api.wheretheiss.at/v1/satellites/25544";
+
+export default function ISSTracker() {
+  const { data, error, isLoading, mutate } = useSWR(URL, fetcher, { refreshInterval: 5000 });
+  console.log(data);
+  if (error) return <div>failed to load</div>;
+  if (isLoading) return <div>loading...</div>;
+
+  return (
+    <main>
+      <Map longitude={data.longitude} latitude={data.latitude} />
+      <Controls longitude={data.longitude} latitude={data.latitude} onRefresh={() => mutate()} />
+    </main>
+  );
+}
