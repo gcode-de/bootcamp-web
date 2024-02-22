c4f9239f896cb88e22f30ae432472a96d3172d37
Samuel Gesang <sgesang@mailo.com>
2024-02-22 15:05:25 +0100

    add update and delete to fish shop

---
+++ Backend/backend-create_products/components/Product/index.js
@@ -0,0 +1,72 @@
+import useSWR from "swr";
+import { useRouter } from "next/router";
+import { ProductCard } from "./Product.styled";
+import Comments from "../Comments";
+import { StyledLink } from "../Link/Link.styled";
+import { useState } from "react";
+import ProductForm from "../ProductForm";
+
+export default function Product() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  const { data, isLoading, mutate } = useSWR(`/api/products/${id}`);
+
+  const [isEditMode, setIsEditMode] = useState(false);
+
+  async function handleEditProduct(event) {
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const productData = Object.fromEntries(formData);
+    const response = await fetch(`/api/products/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(productData),
+    });
+    if (response.ok) {
+      mutate();
+    }
+  }
+
+  async function handleDeleteProduct() {
+    const response = await fetch(`/api/products/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      router.push("/");
+    }
+  }
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  return (
+    <ProductCard>
+      {isEditMode && <ProductForm onSubmit={handleEditProduct} />}
+      <h2>{data.name}</h2>
+      <p>{data._id}</p>
+      <p>Description: {data.description}</p>
+      <p>
+        Price: {data.price} {data.currency}
+      </p>
+      {data.reviews.length > 0 && <Comments reviews={data.reviews} />}
+      <div>
+        <button
+          onClick={() => {
+            setIsEditMode((isEditMode) => !isEditMode);
+          }}
+        >
+          edit
+        </button>
+        <button onClick={handleDeleteProduct}>delete</button>
+      </div>
+      <StyledLink href="/">Back to all</StyledLink>
+    </ProductCard>
+  );
+}
---
+++ Backend/backend-create_products/components/ProductList/index.js
@@ -0,0 +1,32 @@
+import useSWR from "swr";
+import { StyledHeading, StyledList } from "./ProductList.styled";
+import { StyledLink } from "../Link/Link.styled";
+
+export default function ProductList() {
+  const { data, isLoading } = useSWR("/api/products");
+
+  if (isLoading) {
+    return <h1>Loading...</h1>;
+  }
+
+  if (!data) {
+    return;
+  }
+
+  // console.log(data);
+  // const filteredData = data.sort((a, b) => b.name.localCompare(a.name));
+  // console.log(filteredData);
+
+  return (
+    <>
+      <StyledHeading>Available Fishes</StyledHeading>
+      <StyledList>
+        {data.map((product) => (
+          <li key={product._id}>
+            <StyledLink href={`/${product._id}`}>{product.name}</StyledLink>
+          </li>
+        ))}
+      </StyledList>
+    </>
+  );
+}
